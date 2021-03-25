6ecdc96283e8804a2260da65cb063c273bdd42b8
kevintram <thekevintram@gmail.com>
2021-03-25 12:14:26 -0400

    Edits main to use the navigation framework

--- src/TacoStand.java
+++ src/TacoStand.java
@@ -1,5 +1,24 @@
+import menus.MainMenu;
+import navigation.Menu;
+import navigation.MenuController;
+import java.util.Scanner;
+
 public class TacoStand {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        MenuController menuController = MenuController.getInstance();
+        menuController.navigate(new MainMenu());
+
+        Scanner scan = new Scanner(System.in);
+
+        while (!menuController.isEmpty()) {
+            Menu menu = menuController.getCurrMenu();
+
+            menu.print();
+
+            int response = scan.nextInt();
+            menu.choose(response);
+
+            System.out.println();
+        }
     }
 }
